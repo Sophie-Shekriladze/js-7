5d4faf9f39af9ae82ff377ed0cf3528974040ee3
Sophie Shekriladze <55558347+Sophie-Shekriladze@users.noreply.github.com>
2023-03-24 01:56:37 +0400

    add scroll to top

--- script.js
+++ script.js
@@ -25,3 +25,11 @@ let header = document.querySelector('.header')
 //       });
 // })
 
+let scrollButton = document.getElementById('top')
+
+scrollButton.addEventListener('click', () => {
+    window.scrollTo({
+        top: 0,
+        behavior: 'smooth',
+    })
+})
